5f79fd1450a55de085567224f2013dc18facd2e7
Qmun14 <mamunramdhan@gmail.com>
2022-07-25 17:26:15 +0700

    Creating a More Realistic Setup, database, insert & fetch data

---
+++ backend/db.js
@@ -0,0 +1,33 @@
+const mongodb = require('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+const MongoDbUrl = "mongodb+srv://siswa_mdb:P0rP8iSq2ecLM906@cluster0.bdxkx.mongodb.net/shop?retryWrites=true&w=majority";
+
+let _db;
+
+const initDb = callback => {
+    if (_db) {
+        console.log('Database is already initialized!');
+        return callback(null, _db);
+    }
+    MongoClient.connect(MongoDbUrl)
+      .then(client => {
+        _db = client;
+        callback(null, _db);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+}
+
+const getDb = () => {
+    if (!_db) {
+        throw Error("Database not initialized!");
+    }
+    return _db;
+}
+
+module.exports = {
+    initDb,
+    getDb
+}
