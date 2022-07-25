d928ae4ba87b3a4154125e65b1904421c39f5d37
Qmun14 <mamunramdhan@gmail.com>
2022-07-25 17:25:54 +0700

    Creating a More Realistic Setup, database, insert & fetch data

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const mongodb = require('mongodb').MongoClient;
-
 const productRoutes = require('./routes/products');
 const authRoutes = require('./routes/auth');
+const db = require('./db');
 
 const app = express();
 
@@ -27,4 +26,10 @@ app.use((req, res, next) => {
 app.use('/products', productRoutes);
 app.use('/', authRoutes);
 
-app.listen(3100);
+db.initDb((err, db) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(3100);
+  }
+});
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,59 +1,59 @@
 const Router = require('express').Router;
 const mongodb = require('mongodb');
 
-const MongoClient = mongodb.MongoClient;
+const db = require('../db');
 const Decima128 = mongodb.Decimal128;
 
 const router = Router();
 
-const products = [
-  {
-    _id: 'fasdlk1j',
-    name: 'Stylish Backpack',
-    description:
-      'A stylish backpack for the modern women or men. It easily fits all your stuff.',
-    price: 79.99,
-    image: 'http://localhost:3100/images/product-backpack.jpg'
-  },
-  {
-    _id: 'asdgfs1',
-    name: 'Lovely Earrings',
-    description:
-      "How could a man resist these lovely earrings? Right - he couldn't.",
-    price: 129.59,
-    image: 'http://localhost:3100/images/product-earrings.jpg'
-  },
-  {
-    _id: 'askjll13',
-    name: 'Working MacBook',
-    description:
-      'Yes, you got that right - this MacBook has the old, working keyboard. Time to get it!',
-    price: 1799,
-    image: 'http://localhost:3100/images/product-macbook.jpg'
-  },
-  {
-    _id: 'sfhjk1lj21',
-    name: 'Red Purse',
-    description: 'A red purse. What is special about? It is red!',
-    price: 159.89,
-    image: 'http://localhost:3100/images/product-purse.jpg'
-  },
-  {
-    _id: 'lkljlkk11',
-    name: 'A T-Shirt',
-    description:
-      'Never be naked again! This T-Shirt can soon be yours. If you find that buy button.',
-    price: 39.99,
-    image: 'http://localhost:3100/images/product-shirt.jpg'
-  },
-  {
-    _id: 'sajlfjal11',
-    name: 'Cheap Watch',
-    description: 'It actually is not cheap. But a watch!',
-    price: 299.99,
-    image: 'http://localhost:3100/images/product-watch.jpg'
-  }
-];
+// const products = [
+//   {
+//     _id: 'fasdlk1j',
+//     name: 'Stylish Backpack',
+//     description:
+//       'A stylish backpack for the modern women or men. It easily fits all your stuff.',
+//     price: 79.99,
+//     image: 'http://localhost:3100/images/product-backpack.jpg'
+//   },
+//   {
+//     _id: 'asdgfs1',
+//     name: 'Lovely Earrings',
+//     description:
+//       "How could a man resist these lovely earrings? Right - he couldn't.",
+//     price: 129.59,
+//     image: 'http://localhost:3100/images/product-earrings.jpg'
+//   },
+//   {
+//     _id: 'askjll13',
+//     name: 'Working MacBook',
+//     description:
+//       'Yes, you got that right - this MacBook has the old, working keyboard. Time to get it!',
+//     price: 1799,
+//     image: 'http://localhost:3100/images/product-macbook.jpg'
+//   },
+//   {
+//     _id: 'sfhjk1lj21',
+//     name: 'Red Purse',
+//     description: 'A red purse. What is special about? It is red!',
+//     price: 159.89,
+//     image: 'http://localhost:3100/images/product-purse.jpg'
+//   },
+//   {
+//     _id: 'lkljlkk11',
+//     name: 'A T-Shirt',
+//     description:
+//       'Never be naked again! This T-Shirt can soon be yours. If you find that buy button.',
+//     price: 39.99,
+//     image: 'http://localhost:3100/images/product-shirt.jpg'
+//   },
+//   {
+//     _id: 'sajlfjal11',
+//     name: 'Cheap Watch',
+//     description: 'It actually is not cheap. But a watch!',
+//     price: 299.99,
+//     image: 'http://localhost:3100/images/product-watch.jpg'
+//   }
+// ];
 
 // Get list of products products
 router.get('/', (req, res, next) => {
@@ -66,12 +66,8 @@ router.get('/', (req, res, next) => {
   //     queryPage * pageSize
   //   );
   // }
-  MongoClient.connect(
-    "mongodb+srv://siswa_mdb:P0rP8iSq2ecLM906@cluster0.bdxkx.mongodb.net/shop?retryWrites=true&w=majority"
-  )
-    .then((client) => {
-      const products = [];
-      client
+  const products = [];
+  db.getDb()
         .db()
         .collection("products")
         .find().forEach(product => {
@@ -79,21 +75,13 @@ router.get('/', (req, res, next) => {
           products.push(product)
         })
         .then((result) => {
-          client.close();
           res.status(200).json(products);
         })
         .catch((err) => {
           console.log(err);
-          client.close();
           res.status(500).json({ message: 'An error occured.' });
         });
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ message: 'An error occured.' });
-    });
-  
-});
 
 // Get single product
 router.get('/:id', (req, res, next) => {
@@ -110,32 +98,21 @@ router.post('', (req, res, next) => {
     price: Decima128.fromString(req.body.price.toString()), // store this as 128bit decimal in MongoDB
     image: req.body.image
   };
-  MongoClient.connect(
-    "mongodb+srv://siswa_mdb:P0rP8iSq2ecLM906@cluster0.bdxkx.mongodb.net/shop?retryWrites=true&w=majority"
-  )
-    .then((client) => {
-      client
+  db.getDb()
         .db()
         .collection("products")
         .insertOne(newProduct)
         .then((result) => {
           console.log(result);
-          client.close();
           res
             .status(201)
             .json({ message: "Product added", productId: result.insertedId });
         })
         .catch((err) => {
           console.log(err);
-          client.close();
           res.status(500).json({ message: 'An error occured.' });
         });
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ message: 'An error occured.' });
-    });
-});
 
 // Edit existing product
 // Requires logged in user
