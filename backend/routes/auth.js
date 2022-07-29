398ffd7c16bb7c5eb7ba4cb04f732c932ce55d87
Qmun14 <mamunramdhan@gmail.com>
2022-07-29 14:53:46 +0700

    create feature => Adding User Sign In

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -15,11 +15,26 @@ router.post('/login', (req, res, next) => {
   const pw = req.body.password;
   // Check if user login is valid
   // If yes, create token and return it to client
-  const token = createToken();
-  // res.status(200).json({ token: token, user: { email: 'dummy@dummy.com' } });
-  res
+  db.getDb()
+    .db()
+    .collection('users').findOne({email : email})
+    .then( userDoc =>  {
+      return bcrypt.compare(pw, userDoc.password);
+    }).then(result => {
+      if (!result) {
+        throw Error();
+      }
+      const token = createToken();
+      res
+      .status(200)
+      .json({ message: 'Authentication succeded!', token: token });
+    })
+    .catch( err => {
+      res
     .status(401)
     .json({ message: 'Authentication failed, invalid username or password.' });
+    })
+  // res.status(200).json({ token: token, user: { email: 'dummy@dummy.com' } });
 });
 
 router.post('/signup', (req, res, next) => {
